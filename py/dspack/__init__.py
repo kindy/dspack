8743ca7763a8b5fbadeffb71382baa8aa4f676af
Kindy Lin <kindy.lin@gmail.com>
2013-11-08 02:25:29 +0800

    finish fmts build.

--- py/dspack/__init__.py
+++ py/dspack/__init__.py
@@ -1,4 +1,3 @@
-
 import sys
 import re
 from cStringIO import StringIO
@@ -45,16 +44,50 @@ class Field(object):
 
     def __init__(self, typ, fixed=True, len_=None, id_=None, note=None):
         self.typ = typ
+        self.is_str = typ in 'spS'
         self.fixed = fixed
-        self.len = len_
+        if self.is_str:
+            self.len = len_
+        elif typ in 'xcbB?':
+            self.len = 1
+        elif typ in 'hH':
+            self.len = 2
+        elif typ in 'iIlL':
+            self.len = 4
+        elif typ in 'qQfd':
+            self.len = 8
+        else:
+            raise FieldSchemaError('unknow type: %s' % typ)
+
         self.is_list = False
         self.id = id_
         self.note = note
+        self._fmt = None
+
+    @property
+    def fmt(self):
+        '''
+        a tuple (count, format)
+        '''
+        if self._fmt is None:
+            self._fmt = self.build_fmt()
+
+        return self._fmt
+
+    def build_fmt(self):
+        if self.typ not in 'spS':
+            return (1, self.typ)
+
+        if self.fixed:
+            return (1, '%d%s' % (self.len, self.typ))
+
+        # 0 means need to load length from data
+        return (0, self.typ)
 
     def __str__(self):
         return '%s: %s' % (self.id, self.typ)
 
-class ListField(object):
+class ListField(Field):
 
     def __init__(self, typ, fixed=True, len_=None, id_=None, note=None):
         self.is_list = True
@@ -63,6 +96,22 @@ class ListField(object):
         self.len = len_
         self.id = id_
         self.note = note
+        self._fmt = None
+
+    def build_fmt(self):
+        if self.fixed:
+            if not self.typ.is_str:
+                return (self.len, '%d%s' % (self.len, self.typ.typ))
+
+            if self.typ.fixed:
+                return (1, '%ds' % (self.len * self.typ.len))
+
+            return (0, None)
+
+        if (not self.typ.is_str) or self.typ.fixed:
+            return (-1, None)
+
+        return (0, None)
 
     def __str__(self):
         return '%s: %s[]' % (self.id, self.typ.typ)
@@ -88,24 +137,21 @@ class Pack(object):
         dyn string          - <data-len>raw data
         fixed-len list
             fixed-len string- raw data
-            dyn string      - <list-data-len><data-len>raw data<data-len>raw data...
+        (0, None) dyn string - <list-data-len><data-len>raw data<data-len>raw data...
         dyn list
-            fixed-len string- <list-len>raw data
-            dyn string      - <list-data-len><data-len>raw data<data-len>raw data...
+        (-1,None) fixed-len string- <list-len>raw data
+        (0, None) dyn string      - <list-data-len><data-len>raw data<data-len>raw data...
 
         dynamic string || dynamic list || (fixed list with dynamic string)
         '''
         fmt = fmt.lstrip()
         flag = fmt[0]
-        if flag in '@=<>!':
+        if flag in '=<>!':
             fmt = fmt[1:]
         else:
             flag = '!'
 
         self.flag = flag
-
-        _fmts = []
-        _fmt = None
 
         re_field = r'''
             (?P<list>(?P<llen>\d+|~)?:)?
@@ -151,13 +197,39 @@ class Pack(object):
                     if llen < 1:
                         raise FieldSchemaError('length of list: %s should >= 1' % typ)
 
-                _field = ListField(typ=_field, fixed=fixed, len_=len_,
+                _field = ListField(typ=_field, fixed=fixed, len_=llen,
                         id_=id_, note=note)
 
             _fields.append(_field)
 
-        for x in _fields:
-            print x
+        self._fields = tuple(_fields)
+        self._field_ids = tuple(_field.id for _field in _fields)
+
+        self.build_fmts(_fields)
+
+    def build_fmts(self, fields):
+        _fmts = []
+        self._fmts = _fmts
+        _fmt = None
+        last_fixed = None
+        fixed = None
+
+        for field in fields:
+            c, fmt = field.fmt
+            fixed = c > 0
+
+            if fixed != last_fixed:
+                _fmt = [ fixed, [], [] ]
+                _fmts.append(_fmt)
+
+            if fixed:
+                _fmt[1].append(fmt)
+                _fmt[2].append(c)
+            else:
+                _fmt[1].append(c)
+                _fmt[2].append(1)
+
+            last_fixed = fixed
 
 
     def help(self):
@@ -167,11 +239,14 @@ class Pack(object):
         pass
 
     def addRow(self, *rows, **row):
-        if row:
-            self.addKWRow(row)
+        if row and not rows:
+            rows = [ [ row[id_] for id_ in self._field_ids ] ]
+
+        self.addRows(rows, row)
 
     def addRows(self, rows):
-        x
+        for row in rows:
+            print row
 
     def getData(self):
         pass
@@ -186,20 +261,28 @@ def dumps(fmt, rows):
 
 def _test():
     _fmt = '''
-    ! H(day) B(device) B(feed) 3S(country) I(category)
+    !
+    H(day) B(device) B(feed) 3S(country) I(category)
     ~:Q(app_ids) ~:H(changes App's rank change)
+    2:H(f)
+    H(day) B(device)
     '''
     p = Pack(_fmt)
 
+    _data = [
+        [123, 2, 2, 'CN', 123654,
+            [123, 231, 1213], [1, 2, 0]
+        ],
+        [123, 2, 2, 'CN', 123654,
+            [123, 231, 1213], [1, 2, 0]
+        ],
+    ]
+
+    print p._fmts
+    p.addRows(_data)
+
     '''
-    print repr(dumps(, [
-        [123, 2, 2, 'CN', 123654,
-            [123, 231, 1213], [1, 2, 0]
-        ],
-        [123, 2, 2, 'CN', 123654,
-            [123, 231, 1213], [1, 2, 0]
-        ],
-    ]))
+    print repr(dumps(, ))
     '''
 
 _test()
